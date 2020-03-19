e239caa92d4e70b44ff92b7c2a371f748433820d
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-19 19:42:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+counter = 0
+newarray = []
+ while array[counter] < array.length do
+new array << array [counter]**2
+  counter += 1
+  end
+  return newarray
 end
